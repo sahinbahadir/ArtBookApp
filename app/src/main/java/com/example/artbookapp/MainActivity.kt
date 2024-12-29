6e2809d992fdfd1a69b3de4d0b8d0684788fd135
bahadir.sahin <Rauro@DESKTOP-M71F9PI>
2024-12-29 16:58:26 +0300

    Main activity, layout and options menu added

--- app/src/main/java/com/example/artbookapp/MainActivity.kt
+++ app/src/main/java/com/example/artbookapp/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.artbookapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.artbookapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // If toolbar isn't showing on the app set support action bar.
+        setSupportActionBar(binding.mainToolbar)
+
     }
+
+    // Inflates options menu to the layout.
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.addItem -> {
+
+                // Intent
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
